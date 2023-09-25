28109427e4a1cff5cad502236c06854ce72591f2
antoniomrrds <arqueiroverd@gmail.com>
2023-09-25 02:15:04 -0300

    feat: ensure request return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,8 @@
-import { bodyParser, cors } from '../middlewares';
+import { bodyParser, contentType, cors } from '../middlewares';
 import { Express } from 'express';
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser);
   app.use(cors);
+  app.use(contentType);
 };
