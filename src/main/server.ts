3e4d43a192b9139ac420fae42de21e3a894f7e49
antoniomrrds <arqueiroverd@gmail.com>
2023-09-25 01:22:20 -0300

    chore: add express

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,1 +1,7 @@
 import 'module-alias/register';
+import { app } from './config';
+
+const port = process.env.PORT || 5000;
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`),
+);
