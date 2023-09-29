58cc6ee1bdac28973e5e57460c9e7a20c8b6c108
antoniomrrds <arqueiroverd@gmail.com>
2023-09-28 23:04:00 -0300

    test: ensure  getOrDefaultEnvironmentVariable function its valid

---
+++ src/shared/infrastructure/env-config/environment-variable.spec.ts
@@ -0,0 +1,44 @@
+import { getOrDefaultEnvironmentVariable } from '@/shared/infrastructure/env-config/utils';
+import { EnvironmentVariableError } from '@/shared/infrastructure/env-config/errors';
+
+describe('getOrDefaultEnvironmentVariable', () => {
+  beforeEach(() => {
+    process.env.TEST = 'any_value';
+  });
+
+  afterEach(() => {
+    delete process.env.TEST;
+  });
+
+  it('should return the value of an existing environment variable', () => {
+    const result = getOrDefaultEnvironmentVariable('TEST');
+    expect(result).toEqual('any_value');
+  });
+
+  it('should return the default value if the environment variable is not defined', () => {
+    const result = getOrDefaultEnvironmentVariable(
+      'NON_EXISTENT_VARIABLE',
+      'default_value',
+    );
+    expect(result).toEqual('default_value');
+  });
+
+  it('should throw an error if the environment variable is not defined and no default value is provided', () => {
+    expect(() => {
+      getOrDefaultEnvironmentVariable('NON_EXISTENT_VARIABLE');
+    }).toThrow(EnvironmentVariableError);
+  });
+
+  it('should return the default value if the environment variable is empty', () => {
+    process.env.TEST = '';
+    const result = getOrDefaultEnvironmentVariable('TEST', 'default_value');
+    expect(result).toEqual('default_value');
+  });
+
+  it('should throw an error if the environment variable is empty and no default value is provided', () => {
+    process.env.TEST = '';
+    expect(() => {
+      getOrDefaultEnvironmentVariable('TEST');
+    }).toThrow(EnvironmentVariableError);
+  });
+});
