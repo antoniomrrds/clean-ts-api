bc03f126fd77e513c147f6d8e37dd2b90a898d92
antoniomrrds <arqueiroverd@gmail.com>
2023-10-01 04:05:10 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,15 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { LoginController } from '@/presentation/controllers/login';
-import { MissingParamError } from '@/presentation/errors';
+import { InvalidParamError, MissingParamError } from '@/presentation/errors';
 import { badRequest } from '@/presentation/helpers';
+import { EmailValidator } from '@/presentation/ports/email-validator';
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+
+  return new EmailValidatorStub();
+};
 
 type sutTypes = {
   sut: LoginController;
+  emailValidatorStub: EmailValidator;
 };
 
 const makeSut = (): sutTypes => {
-  const sut = new LoginController();
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new LoginController(emailValidatorStub);
   return {
     sut,
+    emailValidatorStub,
   };
 };
 
@@ -30,10 +45,23 @@ describe('Login Controller', () => {
     const { sut } = makeSut();
     const httpRequest = {
       body: {
-        email: 'anyemail@mail.com',
+        email: 'any_email@mail.com',
       },
     };
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
+  });
+
+  it('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
   });
 });
