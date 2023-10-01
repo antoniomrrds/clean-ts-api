b9528a1427e36230c777b9c4707e4e2ab31443c0
antoniomrrds <arqueiroverd@gmail.com>
2023-10-01 04:22:31 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,30 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { InvalidParamError, MissingParamError } from '@/presentation/errors';
-import { badRequest } from '@/presentation/helpers';
+import { badRequest, serverError } from '@/presentation/helpers';
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/ports';
 import { EmailValidator } from '@/presentation/ports/email-validator';
 
 export class LoginController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = request.body;
-    if (!email) {
-      return new Promise(resolve =>
-        resolve(badRequest(new MissingParamError('email'))),
-      );
-    }
-    if (!password) {
-      return new Promise(resolve =>
-        resolve(badRequest(new MissingParamError('password'))),
-      );
-    }
-    const isValid = this.emailValidator.isValid(email);
-    if (!isValid) {
-      return new Promise(resolve =>
-        resolve(badRequest(new InvalidParamError('email'))),
-      );
-    }
+    try {
+      const { email, password } = request.body;
+      if (!email) {
+        return new Promise(resolve =>
+          resolve(badRequest(new MissingParamError('email'))),
+        );
+      }
+      if (!password) {
+        return new Promise(resolve =>
+          resolve(badRequest(new MissingParamError('password'))),
+        );
+      }
+      const isValid = this.emailValidator.isValid(email);
+      if (!isValid) {
+        return new Promise(resolve =>
+          resolve(badRequest(new InvalidParamError('email'))),
+        );
+      }
 
-    return new Promise(resolve => resolve({ statusCode: 200, body: {} }));
+      return new Promise(resolve => resolve({ statusCode: 200, body: {} }));
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
