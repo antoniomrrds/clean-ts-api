ea20cdae4a1193c9992e1cbd712abf1c7368498a
antoniomrrds <arqueiroverd@gmail.com>
2023-10-05 12:54:57 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -6,15 +6,36 @@ import {
   ValidationComposite,
 } from '@/presentation/helpers/validators';
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error {
+      return null as any;
+    }
+  }
+
+  return new ValidationStub();
+};
+
+type SutTypes = {
+  sut: ValidationComposite;
+  validationStub: Validation;
+};
+
+const makesut = (): SutTypes => {
+  const validationStub = makeValidation();
+  const sut = new ValidationComposite([validationStub]);
+  return {
+    sut,
+    validationStub,
+  };
+};
+
 describe('Validation Composite', () => {
   it('should return an error if any validation fails', () => {
-    class ValidationSpy implements Validation {
-      validate(input: any): Error {
-        return new MissingParamError('field');
-      }
-    }
-    const validationSpy = new ValidationSpy();
-    const sut = new ValidationComposite([validationSpy]);
+    const { sut, validationStub } = makesut();
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('field'));
     const error = sut.validate({ field: 'any_value' });
     expect(error).toEqual(new MissingParamError('field'));
   });
