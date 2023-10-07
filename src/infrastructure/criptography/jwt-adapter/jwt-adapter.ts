837105aaab1abf3ca87a89eb2ca42d77593dbfe0
antoniomrrds <arqueiroverd@gmail.com>
2023-10-07 05:56:33 -0300

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infrastructure/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Encrypter } from '@/application/ports/criptography';
+import { sign } from 'jsonwebtoken';
+export class JwtAdapter implements Encrypter {
+  constructor(private readonly secret: string) {}
+  async encrypt(value: string): Promise<string> {
+    await sign({ id: value }, this.secret);
+    return null as any;
+  }
+}
