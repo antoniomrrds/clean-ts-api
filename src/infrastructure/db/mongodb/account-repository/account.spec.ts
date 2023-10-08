da574cf929c220ea4ef1de2688e084c5d14eb518
antoniomrrds <arqueiroverd@gmail.com>
2023-10-07 22:17:33 -0300

    test: ensure AccountMongoRepository return an account on loadByEmail on Success

--- src/infrastructure/db/mongodb/account-repository/account.spec.ts
+++ src/infrastructure/db/mongodb/account-repository/account.spec.ts
@@ -1,10 +1,11 @@
 import { AccountMongoRepository } from '@/infrastructure/db/mongodb/account-repository';
 import { MongoHelper } from '@/infrastructure/db/mongodb/helpers';
+import { Collection } from 'mongodb';
 
 const makeSut = (): AccountMongoRepository => {
   return new AccountMongoRepository();
 };
-
+let accountCollection: Collection;
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string);
@@ -15,17 +16,32 @@ describe('Account Mongo Repository', () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts');
+    accountCollection = await MongoHelper.getCollection('accounts');
     await accountCollection?.deleteMany({});
   });
 
-  it('Should return an account on success', async () => {
+  it('Should return an account on add success', async () => {
     const sut = makeSut();
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password',
     });
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email@mail.com');
+    expect(account.password).toBe('any_password');
+  });
+
+  it('Should return an account on loadByEmail on success', async () => {
+    const sut = makeSut();
+    accountCollection?.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    const account = await sut.loadByEmail('any_email@mail.com');
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
     expect(account.name).toBe('any_name');
