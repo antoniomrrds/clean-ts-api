2890a09866c434b2a4336d13a7633df4079ec59d
antoniomrrds <arqueiroverd@gmail.com>
2023-10-08 23:44:04 -0300

    feat: addd LoginFactory

--- src/shared/infrastructure/env-config/index.ts
+++ src/shared/infrastructure/env-config/index.ts
@@ -1,9 +1,10 @@
 import { getOrDefaultEnvironmentVariable } from '@/shared/infrastructure/env-config/utils';
 
-export const { portServer, mongoUrl } = {
+export const { portServer, mongoUrl, jwtSecret } = {
   portServer: getOrDefaultEnvironmentVariable('PORT', '5000'),
   mongoUrl: getOrDefaultEnvironmentVariable(
     'MONGO_URL',
     'mongodb://127.0.0.1:27017/clean-node-api',
   ),
+  jwtSecret: getOrDefaultEnvironmentVariable('', 't*45-1arf@123'),
 };
