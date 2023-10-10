8cb3823220241791558c8a8acc4aa9b9f59c7643
antoniomrrds <arqueiroverd@gmail.com>
2023-10-10 05:42:45 -0300

    chore: update docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
-FROM node:18.8
+FROM node:18.8.0
 WORKDIR /usr/app/clean-architecture-api
-COPY package*.json ./
-RUN npm install --only=production
-COPY ./dist ./dist
-EXPOSE 5000
-CMD npm start
+COPY ./package*.json .
+RUN npm install --only=dev
+
--- src/shared/infrastructure/env-config/index.ts
+++ src/shared/infrastructure/env-config/index.ts
@@ -4,7 +4,7 @@ export const { portServer, mongoUrl, jwtSecret } = {
   portServer: getOrDefaultEnvironmentVariable('PORT', '5000'),
   mongoUrl: getOrDefaultEnvironmentVariable(
     'MONGO_URL',
-    'mongodb://127.0.0.1:27017/clean-node-api',
+    'mongodb://mongo:27017/clean-node-api',
   ),
   jwtSecret: getOrDefaultEnvironmentVariable('JWT_SECRET', 't*45-1arf@123'),
 };
