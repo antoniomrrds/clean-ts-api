825bb2546160faaaa37ce9ac2f952f730f0ca032
antoniomrrds <arqueiroverd@gmail.com>
2023-11-04 01:09:16 -0300

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct token

---
+++ src/domain/usecases/load-account-by-token.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '@/domain/entities';
+
+export interface LoadAccountByToken {
+  load(accessToken: string, role?: string): Promise<AccountModel>;
+}
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,11 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { LoadAccountByToken } from '@/domain/usecases';
 import { AccessDeniedError } from '@/presentation/errors';
 import { forbidden } from '@/presentation/helpers/http';
 import { HttpRequest, Middleware } from '@/presentation/ports';
 
 export class AuthMiddleware implements Middleware {
+  constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
   async handle(httpRequest: HttpRequest) {
-    const error = forbidden(new AccessDeniedError());
-    return Promise.resolve(error);
+    const accessToken = httpRequest.headers?.['x-access-token'];
+    if (accessToken) {
+      await this.loadAccountByToken.load(accessToken);
+    }
+
+    return forbidden(new AccessDeniedError());
   }
 }
