ea7e358d938643a67b8f572d4b35618faa41d7ac
antoniomrrds <arqueiroverd@gmail.com>
2023-11-04 01:17:29 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -2,11 +2,11 @@
 import { LoadAccountByToken } from '@/domain/usecases';
 import { AccessDeniedError } from '@/presentation/errors';
 import { forbidden } from '@/presentation/helpers/http';
-import { HttpRequest, Middleware } from '@/presentation/ports';
+import { HttpRequest, HttpResponse, Middleware } from '@/presentation/ports';
 
 export class AuthMiddleware implements Middleware {
   constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
-  async handle(httpRequest: HttpRequest) {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.['x-access-token'];
     if (accessToken) {
       await this.loadAccountByToken.load(accessToken);
