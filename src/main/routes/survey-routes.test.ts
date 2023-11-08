ec024b1ce0ba0aaf85fba3949f04122b5814647f
antoniomrrds <arqueiroverd@gmail.com>
2023-11-08 00:20:27 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest';
 import { app } from '@/main/config';
 import { MongoHelper } from '@/infrastructure/db/mongodb/helpers';
 import { Collection } from 'mongodb';
+import { jwtSecret } from '@/shared/infrastructure/env-config';
+import { sign } from 'jsonwebtoken';
 describe('Survey Routes', () => {
   let surveyCollection: Collection;
+  let accountCollection: Collection;
 
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string);
@@ -15,6 +18,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
   });
 
   describe('POST /surveys', () => {
@@ -35,5 +40,48 @@ describe('Survey Routes', () => {
         })
         .expect(403);
     });
+  });
+
+  it('Should return 204 on add survey with valid accessToken', async () => {
+    const result = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+      role: 'admin',
+    });
+
+    const fakeAccount = await accountCollection?.findOne({
+      _id: result?.insertedId,
+    });
+
+    const id = fakeAccount?._id;
+    const accessToken = sign({ id }, jwtSecret);
+    await accountCollection.updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          accessToken,
+        },
+      },
+    );
+
+    await request(app)
+      .post('/api/surveys')
+      .set('x-access-token', accessToken)
+      .send({
+        question: 'Question',
+        answers: [
+          {
+            image: 'http://image-name.com/image.jpg',
+            answer: 'Answer 1',
+          },
+          {
+            answer: 'Answer 2',
+          },
+        ],
+      })
+      .expect(204);
   });
 });
