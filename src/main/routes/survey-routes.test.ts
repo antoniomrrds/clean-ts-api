878b2b0e607f98690a9047b7377db9bc10612114
antoniomrrds <arqueiroverd@gmail.com>
2023-11-08 00:04:51 -0300

    test: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -18,7 +18,7 @@ describe('Survey Routes', () => {
   });
 
   describe('POST /surveys', () => {
-    it('Should return 204 on add survey success', async () => {
+    it('Should return 403 on add survey without accessToken', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -33,7 +33,7 @@ describe('Survey Routes', () => {
             },
           ],
         })
-        .expect(204);
+        .expect(403);
     });
   });
 });
