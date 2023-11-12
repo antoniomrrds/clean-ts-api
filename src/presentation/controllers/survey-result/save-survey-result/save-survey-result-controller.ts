e15ac44f4a3d102f664edac6b3e236f8fb464e4a
antoniomrrds <arqueiroverd@gmail.com>
2023-11-12 01:13:45 -0300

    feat: ensure SaveSurveyResultController  returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -5,19 +5,23 @@ import {
   LoadSurveyById,
 } from '@/presentation/controllers/survey-result/save-survey-result/ports';
 import { InvalidParamError } from '@/presentation/errors';
-import { forbidden } from '@/presentation/helpers/http';
+import { forbidden, serverError } from '@/presentation/helpers/http';
 
 export class SaveSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveyResult = await this.loadSurveyById.loadById(
-      httpRequest.params.surveyId,
-    );
-    if (!surveyResult) {
-      return forbidden(new InvalidParamError('surveyId'));
-    }
+    try {
+      const surveyResult = await this.loadSurveyById.loadById(
+        httpRequest.params.surveyId,
+      );
+      if (!surveyResult) {
+        return forbidden(new InvalidParamError('surveyId'));
+      }
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return null as any;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return null as any;
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
