ad01917427e81da14e52260fa31912a15067049f
antoniomrrds <arqueiroverd@gmail.com>
2023-11-12 01:05:36 -0300

    feat: ensure SaveSurveyResultController return 403 if invalid survey is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -4,11 +4,19 @@ import {
   HttpResponse,
   LoadSurveyById,
 } from '@/presentation/controllers/survey-result/save-survey-result/ports';
+import { InvalidParamError } from '@/presentation/errors';
+import { forbidden } from '@/presentation/helpers/http';
 
 export class SaveSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyById.loadById(httpRequest.params.surveyId);
+    const surveyResult = await this.loadSurveyById.loadById(
+      httpRequest.params.surveyId,
+    );
+    if (!surveyResult) {
+      return forbidden(new InvalidParamError('surveyId'));
+    }
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return null as any;
   }
