4a1f09f636ebef1e934986f8bf2e8fe4af2e1326
antoniomrrds <arqueiroverd@gmail.com>
2023-11-15 20:57:06 -0300

    feat: ensure IdParamValidation return an error if ObjectIdValidator returns false

--- src/validation/validators/id-param-validation.ts
+++ src/validation/validators/id-param-validation.ts
@@ -1,12 +1,16 @@
+import { InvalidParamError } from '@/presentation/errors';
 import { Validation } from '@/presentation/ports';
 import { ObjectIdValidator } from '@/validation/ports';
 
 export class IdParamValidation implements Validation {
-  constructor(private readonly objectIdValidator: ObjectIdValidator) {}
-
+  constructor(
+    private readonly fieldName: string,
+    private readonly objectIdValidator: ObjectIdValidator,
+  ) {}
   validate(id: string): Error | undefined {
-    this.objectIdValidator.isValidObjectId(id);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return null as any;
+    const isValid = this.objectIdValidator.isValidObjectId(id);
+    if (!isValid) {
+      return new InvalidParamError(this.fieldName);
+    }
   }
 }
