bac2e70fb90b73c47a93a511e0d1700e77fd731b
antoniomrrds <arqueiroverd@gmail.com>
2023-11-15 22:28:35 -0300

    test: ensure MongoIdValidatorAdapter return true if MongoHelper.isValidObjectId returns true

--- src/infrastructure/db/mongodb/validation/validators/mongo-id-validator-adapter.spec.ts
+++ src/infrastructure/db/mongodb/validation/validators/mongo-id-validator-adapter.spec.ts
@@ -1,6 +1,8 @@
 import { MongoHelper } from '@/infrastructure/db/mongodb/helpers';
 import { MongoIdValidatorAdapter } from '@/infrastructure/db/mongodb/validation/validators';
 
+jest.mock('@/infrastructure/db/mongodb/helpers');
+
 const makeSut = (): MongoIdValidatorAdapter => {
   return new MongoIdValidatorAdapter();
 };
@@ -13,5 +15,13 @@ describe('MongoIdValidator Adapter', () => {
     const isValid = sut.isValidId('invalidObjectId');
 
     expect(isValid).toBe(false);
+  });
+  it('Should return true if MongoHelper.isValidObjectId returns true', () => {
+    const sut = makeSut();
+    jest.spyOn(MongoHelper, 'isValidObjectId').mockReturnValueOnce(true);
+
+    const isValid = sut.isValidId('validObjectId');
+
+    expect(isValid).toBe(true);
   });
 });
