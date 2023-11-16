6dc56cf0a71602b4c4f2f3c9301b8dfe34b99606
antoniomrrds <arqueiroverd@gmail.com>
2023-11-15 23:48:24 -0300

    test: add ObjectId validity checks in mongo-helper.spec.ts

--- src/infrastructure/db/mongodb/helpers/mongo-helper.spec.ts
+++ src/infrastructure/db/mongodb/helpers/mongo-helper.spec.ts
@@ -1,4 +1,5 @@
 import { MongoHelper as sut } from '@/infrastructure/db/mongodb/helpers/mongo-helper';
+import { ObjectId } from 'mongodb';
 
 describe('Mongo Helper', () => {
   beforeAll(async () => {
@@ -16,4 +17,14 @@ describe('Mongo Helper', () => {
     await sut.getCollection('accounts');
     expect(sut.isConnected).toBe(true);
   });
+});
+it('Should return true if ObjectId is valid', () => {
+  const objectId = new ObjectId();
+  const isValid = sut.isValidObjectId(objectId.toHexString());
+  expect(isValid).toBe(true);
+});
+
+it('Should return false if ObjectId is invalid', () => {
+  const isValid = sut.isValidObjectId('invalid_id');
+  expect(isValid).toBe(false);
 });
