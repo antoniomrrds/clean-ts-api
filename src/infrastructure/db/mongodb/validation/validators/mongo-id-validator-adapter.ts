d3196b400887d6f7ff2ebc7ef9df07673411447b
antoniomrrds <arqueiroverd@gmail.com>
2023-11-15 23:38:09 -0300

    feat: ensure MongoIdValidatorAdapter return false if MongoHelper.isValidObjectId throws an error

--- src/infrastructure/db/mongodb/validation/validators/mongo-id-validator-adapter.ts
+++ src/infrastructure/db/mongodb/validation/validators/mongo-id-validator-adapter.ts
@@ -1,8 +1,11 @@
 import { MongoHelper } from '@/infrastructure/db/mongodb/helpers';
 import { IdValidator } from '@/validation/ports';
 export class MongoIdValidatorAdapter implements IdValidator {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   isValidId(id: string): boolean {
-    return MongoHelper.isValidObjectId(id);
+    try {
+      return MongoHelper.isValidObjectId(id);
+    } catch (error) {
+      return false;
+    }
   }
 }
