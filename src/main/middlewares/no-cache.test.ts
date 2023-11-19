94c62fe79f26379b46a669d84d7e80c112791fae
antoniomrrds <arqueiroverd@gmail.com>
2023-11-19 05:36:00 -0300

    test: add no cahe middleware

---
+++ src/main/middlewares/no-cache.test.ts
@@ -0,0 +1,20 @@
+import { app } from '@/main/config';
+import { noCahe } from '@/main/middlewares';
+import request from 'supertest';
+
+describe('No Cache Middleware', () => {
+  test('Should disable cache', async () => {
+    app.get('/test_no_cache', noCahe, (req, res) => {
+      res.send();
+    });
+    await request(app)
+      .get('/test_no_cache')
+      .expect(
+        'cache-control',
+        'no-store, no-cache, must-revalidate, proxy-revalidate',
+      )
+      .expect('pragma', 'no-cache')
+      .expect('expires', '0')
+      .expect('surrogate-control', 'no-store');
+  });
+});
