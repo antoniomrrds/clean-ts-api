9d033e98754783bf7a5f645aab14b96230756023
antoniomrrds <arqueiroverd@gmail.com>
2023-11-19 14:44:33 -0300

    chore: api login docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,30 @@
+import { loginPath } from '@/main/docs/paths';
+import { accountSchema, loginParamsSchema } from '@/main/docs/schemas';
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node TS API',
     description: 'API para realizar enquetes entre programadores',
     version: '1.0.0',
+  },
+  servers: [
+    {
+      url: '/api',
+      description: 'Servidor principal',
+    },
+  ],
+  tags: [
+    {
+      name: 'Login',
+      description: 'APIs relacionadas a Login',
+    },
+  ],
+  paths: {
+    '/login': loginPath,
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
   },
 };
