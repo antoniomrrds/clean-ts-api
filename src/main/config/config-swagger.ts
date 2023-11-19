414fbcc57c9879483387c6aec2df16425c3ac433
antoniomrrds <arqueiroverd@gmail.com>
2023-11-19 05:51:27 -0300

    feat: apply no-cahe middleware on swagger api

--- src/main/config/config-swagger.ts
+++ src/main/config/config-swagger.ts
@@ -1,7 +1,8 @@
 import { Express } from 'express';
 import swaggerConfig from '@/main/docs';
 import { serve, setup } from 'swagger-ui-express';
+import { noCahe } from '@/main/middlewares';
 
 export default (app: Express): void => {
-  app.use('/api-docs', serve, setup(swaggerConfig));
+  app.use('/api-docs', noCahe, serve, setup(swaggerConfig));
 };
