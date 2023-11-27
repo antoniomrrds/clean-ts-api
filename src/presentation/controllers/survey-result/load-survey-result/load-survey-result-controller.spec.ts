93c024a5e4975693422cfcfba521191652eb7597
antoniomrrds <arqueiroverd@gmail.com>
2023-11-27 02:01:06 -0300

    test: ensure LoadSurveyResultController calls validation with correct id

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -4,10 +4,12 @@ import { LoadSurveyResultController } from '@/presentation/controllers/survey-re
 import {
   HttpRequest,
   LoadSurveyById,
+  Validation,
 } from '@/presentation/controllers/survey-result/load-survey-result/ports';
 import { InvalidParamError } from '@/presentation/errors';
 import { forbidden, serverError } from '@/presentation/helpers/http';
 import { mockLoadSurveyByIdStub } from '@/presentation/test';
+import { mockValidation } from '@/validation/test';
 
 const mockFakeRequest = (): HttpRequest => ({
   params: {
@@ -18,17 +20,35 @@ const mockFakeRequest = (): HttpRequest => ({
 type SutTypes = {
   sut: LoadSurveyResultController;
   loadSurveyByIdStub: LoadSurveyById;
+  validationStub: Validation;
 };
 
 const makeSut = (): SutTypes => {
   const loadSurveyByIdStub = mockLoadSurveyByIdStub();
-  const sut = new LoadSurveyResultController(loadSurveyByIdStub);
+  const validationStub = mockValidation();
+
+  const sut = new LoadSurveyResultController(
+    loadSurveyByIdStub,
+    validationStub,
+  );
   return {
     sut,
     loadSurveyByIdStub,
+    validationStub,
   };
 };
 describe('LoadSurveyResult Controller', () => {
+  it('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut();
+
+    const validateSpy = jest.spyOn(validationStub, 'validate');
+
+    const httpRequest = mockFakeRequest();
+
+    await sut.handle(httpRequest);
+
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.params.surveyId);
+  });
   it('should call LoadSurveyById with correct id', () => {
     const { sut, loadSurveyByIdStub } = makeSut();
     const loadSpy = jest.spyOn(loadSurveyByIdStub, 'loadById');
