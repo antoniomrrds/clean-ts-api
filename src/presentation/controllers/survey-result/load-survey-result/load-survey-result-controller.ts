fd22e132591313fff99549b2dd7274868f91132a
antoniomrrds <arqueiroverd@gmail.com>
2023-11-27 01:53:16 -0300

    feat: ensure LoadSurveyResultController returns 500 if LoadSurveById throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -5,19 +5,23 @@ import {
   LoadSurveyById,
 } from '@/presentation/controllers/survey-result/load-survey-result/ports';
 import { InvalidParamError } from '@/presentation/errors';
-import { forbidden } from '@/presentation/helpers/http';
+import { forbidden, serverError } from '@/presentation/helpers/http';
 
 export class LoadSurveyResultController implements Controller {
   constructor(private readonly loadSurveyById: LoadSurveyById) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const survey = await this.loadSurveyById.loadById(
-      httpRequest.params.surveyId,
-    );
-    if (!survey) {
-      return forbidden(new InvalidParamError('surveyId'));
-    }
+    try {
+      const survey = await this.loadSurveyById.loadById(
+        httpRequest.params.surveyId,
+      );
+      if (!survey) {
+        return forbidden(new InvalidParamError('surveyId'));
+      }
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return Promise.resolve(null as any);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return Promise.resolve(null as any);
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
