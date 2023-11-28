5a24ddf789fee57ea6065805a9b3830a03cb5e49
antoniomrrds <arqueiroverd@gmail.com>
2023-11-28 04:29:50 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -2,6 +2,8 @@ export const surveyResultPath = {
   put: {
     tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
+    description:
+      'Essa rota só pode ser executada por **usuários autenticados**',
     security: [
       {
         apiKeyAuth: [],
@@ -11,6 +13,7 @@ export const surveyResultPath = {
       {
         in: 'path',
         name: 'surveyId',
+        description: 'ID da enquete a ser respondida',
         required: true,
         schema: {
           type: 'string',
@@ -26,6 +29,51 @@ export const surveyResultPath = {
         },
       },
     },
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      400: {
+        $ref: '#/components/badRequest',
+      },
+      403: {
+        $ref: '#/components/forbidden',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
+  get: {
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description: 'Essa rota pode ser executada por **usuários autenticados**',
+    security: [
+      {
+        apiKeyAuth: [],
+      },
+    ],
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        description: 'ID da enquete a ser respondida',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
     responses: {
       200: {
         description: 'Sucesso',
