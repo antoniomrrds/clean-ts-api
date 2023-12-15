ffdfdebc8fd7bf675ac8e8275918274dcd0a58f9
antoniomrrds <arqueiroverd@gmail.com>
2023-12-15 04:05:10 -0300

    feat: ✨add signUp mutation

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { adaptResolver } from '@/main/adapters';
-import { makeLoginController } from '@/main/factories';
+import { makeLoginController, makeSignUpController } from '@/main/factories';
 
 export default {
   Query: {
     login: async (parent: any, args: any) =>
       await adaptResolver(makeLoginController(), args),
+  },
+  Mutation: {
+    signup: async (parent: any, args: any) =>
+      await adaptResolver(makeSignUpController(), args),
   },
 };
--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -2,6 +2,9 @@ export default `#graphql
   extend type Query {
     login(email: String!, password: String!): Account!
   }
+  extend type Mutation {
+    signup(name: String!, email: String!, password: String!, passwordConfirmation: String!): Account!
+  }
   type Account {
     accessToken: String!
     name: String!
